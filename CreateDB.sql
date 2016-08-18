98ef10077e8774160af073f046e6e98a3b6ed92e
VikaShulzhitskaya <vikaprevet@gmail.com>
2016-08-18 14:41:19 +0300

    Added changes in the titles of the table columns and added LEFT JOIN in SQL query #11 for displaying employees without work experience

--- CreateDB.sql
+++ CreateDB.sql
@@ -5,74 +5,75 @@ USE Company
 GO
 
 CREATE TABLE Department(
-	DepartmentID int NOT NULL PRIMARY KEY IDENTITY(1,1),
-	DepartmentName nvarchar(100) NOT NULL,
-	DepartmentAddress nvarchar(100)NOT NULL
+	DepartmentId int NOT NULL PRIMARY KEY IDENTITY(1,1),
+	Name nvarchar(100) NOT NULL,
+	Address nvarchar(100)NOT NULL
 )
 
 CREATE TABLE Employee(
-	EmployeeID int NOT NULL PRIMARY KEY IDENTITY(1,1),
-	EmployeeLastName nvarchar(100) NOT NULL,
-	EmployeeFirstName nvarchar(100) NOT NULL,
-	EmployeeDateOfBirth date NOT NULL
+	EmployeeId int NOT NULL PRIMARY KEY IDENTITY(1,1),
+	LastName nvarchar(100) NOT NULL,
+	FirstName nvarchar(100) NOT NULL,
+	DateOfBirth date NOT NULL
 )
 
 CREATE TABLE Job(
-	JobID int NOT NULL PRIMARY KEY IDENTITY(1,1),
-	JobName nvarchar(100) NOT NULL,
+	JobId int NOT NULL PRIMARY KEY IDENTITY(1,1),
+	Name nvarchar(100) NOT NULL,
 	MinSalary money NULL
 )
 
 CREATE TABLE Career(
-	CareerID int NOT NULL PRIMARY KEY IDENTITY(1,1),
-	EmployeeID int NOT NULL,
-	JobID int NOT NULL,
-	DepartmentID int NOT NULL,
+	CareerId int NOT NULL PRIMARY KEY IDENTITY(1,1),
+	EmployeeId int NOT NULL,
+	JobId int NOT NULL,
+	DepartmentId int NOT NULL,
 	HireDate date NOT NULL,
 	DismissalDate date NULL
 )
+GO
 
-ALTER TABLE Career ADD CONSTRAINT FK_Career_EmployeeID
-FOREIGN KEY (EmployeeID) REFERENCES Employee(EmployeeID)
+ALTER TABLE Career ADD CONSTRAINT FK_Career_EmployeeId
+FOREIGN KEY (EmployeeId) REFERENCES Employee(EmployeeId)
 
-ALTER TABLE Career ADD CONSTRAINT FK_Career_JobID
-FOREIGN KEY (JobID) REFERENCES Job(JobID)
+ALTER TABLE Career ADD CONSTRAINT FK_Career_JobId
+FOREIGN KEY (JobId) REFERENCES Job(JobId)
 
-ALTER TABLE Career ADD CONSTRAINT FK_Career_DepartmentID
-FOREIGN KEY (DepartmentID) REFERENCES Department(DepartmentID)
+ALTER TABLE Career ADD CONSTRAINT FK_Career_DepartmentId
+FOREIGN KEY (DepartmentId) REFERENCES Department(DepartmentId)
 
 CREATE TABLE Salary(
-	SalaryID int NOT NULL PRIMARY KEY IDENTITY(1,1),
-	EmployeeID int NOT NULL,
-	SalaryMonth int NOT NULL,
-	SalaryYear int NOT NULL,
+	SalaryId int NOT NULL PRIMARY KEY IDENTITY(1,1),
+	EmployeeId int NOT NULL,
+	Month int NOT NULL,
+	Year int NOT NULL,
 	EmployeeSalary money NOT NULL
 )
 
-ALTER TABLE Salary ADD CONSTRAINT FK_Salary_EmployeeID
-FOREIGN KEY (EmployeeID) REFERENCES Employee(EmployeeID)
+ALTER TABLE Salary ADD CONSTRAINT FK_Salary_EmployeeId
+FOREIGN KEY (EmployeeId) REFERENCES Employee(EmployeeId)
 
 ALTER TABLE Salary
-ADD CONSTRAINT chk_SalaryMonth CHECK (SalaryMonth>0 AND SalaryMonth<13)
+ADD CONSTRAINT chk_Month CHECK (Month>0 AND Month<13)
 
 ALTER TABLE Salary
-ADD CONSTRAINT chk_SalaryYear CHECK (SalaryYear>2002 AND SalaryYear<2016)
+ADD CONSTRAINT chk_Year CHECK (Year>2002 AND Year<2016)
 
-INSERT INTO Department(DepartmentName, DepartmentAddress)
+INSERT INTO Department(Name, Address)
 VALUES('D1', 'Room 1101'),
 	('D11', 'Room 1102'),
 	('D2', 'Room 1201'),
 	('D3', 'Room 1301'),
 	('D4', 'Room 1401')
 
-INSERT INTO Employee(EmployeeLastName, EmployeeFirstName, EmployeeDateOfBirth)
+INSERT INTO Employee(LastName, FirstName, DateOfBirth)
 VALUES('Ivanov','Ivan','19600502'),
 	('Petrov','Peter','19830626'),
 	('Sidorov','Viktor','19571213'),
 	('Kotova','Svetlana','19910322'),
 	('Medvedeva','Alena','19871124')
 
-INSERT INTO Job(JobName, MinSalary)
+INSERT INTO Job(Name, MinSalary)
 VALUES('Chief Engineer',700),
 	('Technologist',500),
 	('Operator',450),
@@ -80,14 +81,14 @@ VALUES('Chief Engineer',700),
 	('Software developer',1200),
 	('HR Director',1500)
 
-INSERT INTO Career(DepartmentID, EmployeeID, JobID, HireDate, DismissalDate)
+INSERT INTO Career(DepartmentId, EmployeeId, JobId, HireDate, DismissalDate)
 VALUES(1,1,6,'20030204',null),
 	(1,2,5,'20140401',null),
 	(4,3,4,'20050911',null),
 	(4,4,1,'20030115','20150610'),
 	(4,5,3,'20120206','20150910')
 
-INSERT INTO Salary(EmployeeID, SalaryMonth, SalaryYear, EmployeeSalary)
+INSERT INTO Salary(EmployeeId, Month, Year, EmployeeSalary)
 VALUES(1,1,2015,1500),
 	(1,2,2015,1500),
 	(1,3,2015,1500),
--- SQLQuery.sql
+++ SQLQuery.sql
@@ -6,99 +6,99 @@ FROM [dbo].[Employee]
 
 --2)	Получить коды и названия должностей, чья минимальная зарплата не превышает 500
 SELECT [JobID], 
-	[JobName] 
+	[Name] 
 FROM [dbo].[Job] 
-WHERE [MinSalary]<=500
+WHERE [MinSalary] <= 500
 
 --3)	Получить среднюю заработную плату начисленную в январе 2015 года
 SELECT AVG(EmployeeSalary) AS AverageSalary
 FROM [dbo].[Salary] s
-WHERE s.[SalaryMonth] = 1 AND s.[SalaryYear] = 2015
+WHERE s.[Month] = 1 AND s.[Year] = 2015
 
 --4)	Получить имя самого старого работника, а также его возраст
-SELECT e.[EmployeeFirstName], 
-	DATEDIFF(YEAR,e.[EmployeeDateOfBirth],GETDATE()) AS Age 
+SELECT e.[FirstName], 
+	DATEDIFF(YEAR, e.[DateOfBirth], GETDATE()) AS Age 
 FROM [dbo].[Employee] e
-WHERE e.[EmployeeDateOfBirth] = (SELECT MIN([EmployeeDateOfBirth]) FROM [dbo].[Employee])
+WHERE e.[DateOfBirth] = (SELECT MIN([DateOfBirth]) FROM [dbo].[Employee])
 
 --5)	Найти фамилии работников, которым была начислена зарплата в январе 2015 года
-SELECT e.[EmployeeLastName] 
+SELECT e.[LastName] 
 FROM [dbo].[Employee] e 
-WHERE e.[EmployeeID] IN
-	(SELECT DISTINCT s.[EmployeeID] 
+WHERE e.[EmployeeId] IN
+	(SELECT DISTINCT s.[EmployeeId] 
 		FROM [dbo].[Salary] s 
-		WHERE s.[SalaryMonth] = 1 AND s.[SalaryYear] = 2015
+		WHERE s.[Month] = 1 AND s.[Year] = 2015
 	)
 
 --6)	Найти коды работников, зарплата которых в мае 2015 года снизилась по сравнению с каким-либо предыдущим месяцем этого же года
-SELECT s.[EmployeeID] 
+SELECT s.[EmployeeId] 
 FROM [dbo].[Salary] s 
-WHERE s.[SalaryMonth] = 5 
-	AND s.[SalaryYear] = 2015 
+WHERE s.[Month] = 5 
+	AND s.[Year] = 2015 
 	AND s.[EmployeeSalary] < ANY
-		(SELECT sl.EmployeeSalary 
+		(SELECT sl.[EmployeeSalary] 
 			FROM [dbo].[Salary] sl 
-			WHERE s.EmployeeID = sl.EmployeeID
-				AND sl.[SalaryYear] = 2015 
-				AND sl.[SalaryMonth]<5 
+			WHERE s.EmployeeId = sl.EmployeeId
+				AND sl.[Year] = 2015 
+				AND sl.[Month] < 5 
 		)
 
 --7)	Получить информацию о кодах, названиях отделов и количестве работающих в этих отделах в настоящее время сотрудников
-SELECT d.[DepartmentID], 
-	d.[DepartmentName],
-	(SELECT COUNT(EmployeeID) 
+SELECT d.[DepartmentId], 
+	d.[Name],
+	(SELECT COUNT(EmployeeId) 
 		FROM [dbo].[Career] c 
 		WHERE c.[DismissalDate] is null 
-			AND c.[DepartmentID] = d.[DepartmentID]
+			AND c.[DepartmentId] = d.[DepartmentId]
 	) AS NumberOfEmployees 
 FROM [dbo].[Department] d
 
 --8)	Найти среднюю начисленную зарплату за 2015 год в разрезе работников
-SELECT s.[EmployeeID],
-	e.[EmployeeLastName],
-	e.[EmployeeFirstName], 
+SELECT s.[EmployeeId],
+	e.[LastName],
+	e.[FirstName], 
 	AVG(s.EmployeeSalary) AS AverageSalary 
 FROM [dbo].[Salary] s 
 JOIN [dbo].[Employee] e 
-ON s.[EmployeeID] = e.[EmployeeID]
-WHERE s.[SalaryYear] = 2015 
-GROUP BY s.[EmployeeID], e.[EmployeeLastName],e.[EmployeeFirstName]
+ON s.[EmployeeId] = e.[EmployeeId]
+WHERE s.[Year] = 2015 
+GROUP BY s.[EmployeeId], e.[LastName], e.[FirstName]
 
 --9)	Найти среднюю зарплату за 2015 год в разрезе работников. Включать в результат только тех работников, начисления которым проводились не менее двух раз
-SELECT s.[EmployeeID],
-	e.[EmployeeLastName],
-	e.[EmployeeFirstName], 
+SELECT s.[EmployeeId],
+	e.[LastName],
+	e.[FirstName], 
 	AVG(s.EmployeeSalary) AS AverageSalary 
 FROM [dbo].[Salary] s 
 JOIN [dbo].[Employee] e 
-ON s.[EmployeeID] = e.[EmployeeID]
-WHERE s.[SalaryYear] = 2015 
-GROUP BY s.[EmployeeID], e.[EmployeeLastName],e.[EmployeeFirstName]
-HAVING COUNT(s.[SalaryMonth]) >= 2
+ON s.[EmployeeId] = e.[EmployeeId]
+WHERE s.[Year] = 2015 
+GROUP BY s.[EmployeeId], e.[LastName], e.[FirstName]
+HAVING COUNT(s.[Month]) >= 2
 
 --10)	Найти имена тех работников, начисленная зарплата которых за январь 2015 превысила 1000
-SELECT e.[EmployeeLastName], 
-	e.[EmployeeFirstName],
+SELECT e.[LastName], 
+	e.[FirstName],
 	s.[EmployeeSalary] AS Salary 
 FROM [dbo].[Employee] e
 JOIN [dbo].[Salary] s
-ON e.[EmployeeID] = s.[EmployeeID]
-WHERE s.[SalaryYear] = 2015 
-	AND s.[SalaryMonth] = 1
+ON e.[EmployeeId] = s.[EmployeeId]
+WHERE s.[Year] = 2015 
+	AND s.[Month] = 1
 	AND s.[EmployeeSalary] > 1000
 
 
 --11)	Найти имена работников и стаж их непрерывной работы (на одной должности и в одном отделе). 
-SELECT e.[EmployeeLastName],
-	e.[EmployeeFirstName], 
-	DATEDIFF(YEAR,c.[HireDate],ISNULL(c.[DismissalDate],GETDATE())) AS WorkExperience 
+SELECT e.[LastName],
+	e.[FirstName], 
+	DATEDIFF(YEAR, c.[HireDate], ISNULL(c.[DismissalDate], GETDATE())) AS WorkExperience 
 FROM [dbo].[Employee] e
-JOIN [dbo].[Career] c 
-ON c.[EmployeeID] = e.[EmployeeID]
+LEFT JOIN [dbo].[Career] c 
+ON c.[EmployeeId] = e.[EmployeeId]
 
 --12)	Увеличить минимальную зарплату для всех должностей в 1.5 раза
 UPDATE [dbo].[Job] SET [MinSalary] = [MinSalary]*1.5
 
 --13)	Удалить из таблицы salary все записи за 2014 и более ранние годы
 
-DELETE [dbo].[Salary] WHERE [SalaryYear] <= 2014
+DELETE [dbo].[Salary] WHERE [Year] <= 2014
